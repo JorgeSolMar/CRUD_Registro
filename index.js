227e6c1988b1e8fc3369bd59c8fb2c3a20b753ee
JorgeSolMar <jorge.solmar12@gmail.com>
2023-01-07 19:08:21 -0600

    se inicia con el evento de editar, se regresa la informacion al formulario

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ let MostrarDB = () => {
     
 
     arrayRegistros.forEach((element, index) => {
-    console.log(index)
+    
     listaRegistrosUI.innerHTML += `<tr>
     <th scope="row">${element.nomyap}</th>
     <td>${element.correo}</td>
     <td>${element.telefono}</td>
     <td>
-        <button type="button" class="btn btn-success">Editar</button>
+        <button onclick='editarDB(${index})' type="button" class="btn btn-success">Editar</button>
         <button onclick='eliminarDB(${index})' type="button" class="btn btn-danger">Eliminar</button>
     </td>
   </tr>`
@@ -47,13 +47,44 @@ let eliminarDB = (position) => {
     GuardarDB();
 }
 
+let editarDB = (position) => {
+
+    listaRegistrosUI.addEventListener('click', (e) => {
+        e.preventDefault();
+        // console.log(e.target.innerHTML);
+
+        if(e.target.innerHTML === 'Editar'){
+        
+            let textonombre = e.path[2].childNodes[1].innerHTML;
+            let textocorreo = e.path[2].childNodes[3].innerHTML;
+            let textotelefono = e.path[2].childNodes[5].innerHTML;
+
+            document.getElementById('nomyap').value = textonombre;
+            document.getElementById('correo').value = textocorreo;
+            document.getElementById('telefono').value = textotelefono;
+
+        }    
+    
+    
+    
+    MostrarDB();
+});
+
+}
+
+
+
+
+// map((elemento, position) =>{
+//     return elemento + 1;
+//     });
 
 
 
 //Eventos
 formularioUI.addEventListener('submit', (e) => {
     e.preventDefault();
-   
+    
     let nomyapUI = document.querySelector('#nomyap').value;
     let correoUI = document.querySelector('#correo').value;
     let telefonoUI = document.querySelector('#telefono').value;
@@ -70,3 +101,4 @@ formularioUI.addEventListener('submit', (e) => {
 });
 
 document.addEventListener('DOMContentLoaded', MostrarDB);
+
